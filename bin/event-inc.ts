bc8a89f9164ef9690b443b7eddd247923198a6f4
Vlad Neculai <v.neculai@exporo.com>
2022-12-29 17:55:42 +0100

    initial setup && ci/cd

---
+++ bin/event-inc.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { EventIncStack } from '../lib/event-inc-stack';
+
+const app = new cdk.App();
+new EventIncStack(app, 'EventIncStack');
